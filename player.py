dcceee0db3424d83b5a1b264eb866d58b757fd95
JR <jrpatton1234@gmail.com>
2024-12-27 23:09:07 -0500

    Add ability for player to rotate

--- player.py
+++ player.py
@@ -2,6 +2,7 @@
 
 import pygame
 from circleshape import CircleShape
+from constants import PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     """Class for the player character"""
@@ -10,7 +11,7 @@ class Player(CircleShape):
         self.rotation = 0
 
     def triangle(self):
-        """Function for triangle representing character"""
+        """Method for triangle representing character"""
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
@@ -19,5 +20,18 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        """Function to draw the player"""
+        """Method to draw the player"""
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        """Method to rotate the character model"""
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        """Method to interact with character model"""
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
